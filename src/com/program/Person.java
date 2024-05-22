8290d8863013a4d8ade5bd45c12abd67da0186f2
viraj phalke <virajphalke20@gmail.com>
2024-05-22 12:50:16 +0530

    My first commit

---
+++ src/com/program/Person.java
@@ -0,0 +1,14 @@
+package com.program;
+
+public class Person {
+	public static void main(String[] args) {
+		Person p=new Person();
+		p.m1();
+		
+	}
+	public void m1()
+	{
+		System.out.println("hellow i am doing coding");
+	}
+
+}
