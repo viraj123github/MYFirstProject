490160abe9fbdf2460b3394ff9bc4ce290b52db4
viraj phalke <virajphalke20@gmail.com>
2024-05-23 11:11:43 +0530

    Add code for Employee And Student

---
+++ src/com/program/Employee.java
@@ -0,0 +1,10 @@
+package com.program;
+
+public class Employee {
+
+	public static void main(String[] args) {
+	System.out.println("i am employee class main method");
+
+	}
+
+}
---
+++ src/com/program/Student.java
@@ -0,0 +1,10 @@
+package com.program;
+
+public class Student {
+
+	public static void main(String[] args) {
+		System.out.println("i am student class main method");
+
+	}
+
+}
