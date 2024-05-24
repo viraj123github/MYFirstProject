ef99835f8ca6061806036afe55d80c294c4404e9
viraj phalke <virajphalke20@gmail.com>
2024-05-24 10:18:11 +0530

    this is code for salary

---
+++ src/com/program/Salary.java
@@ -0,0 +1,11 @@
+package com.program;
+
+public class Salary {
+
+	public static void main(String[] args) {
+     System.out.println("salary should get credited in your account");
+     System.out.println("now currently you have 45000 salary");
+
+	}
+
+}
