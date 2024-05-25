2ecfc55f37739c849bb7fdda7e277f1371ae9c8e
viraj phalke <virajphalke20@gmail.com>
2024-05-25 12:35:02 +0530

    addedd JDBC

--- src/com/program/Cat.java
+++ src/com/program/Cat.java
@@ -5,6 +5,8 @@ public class Cat {
 	public static void main(String[] args) {
 		System.out.println("eat");
 		System.out.println("walk");
+		System.out.println("this is viaj cay");
+		System.out.println("Addedd JDBC");
 
 	}
 
