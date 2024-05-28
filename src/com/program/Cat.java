5159cf272f629bb728ec5d51ff6b3faae9e9950d
viraj123github <virajphalke20@gmail.com>
2024-05-28 10:26:26 +0530

    added conflict message

--- src/com/program/Cat.java
+++ src/com/program/Cat.java
@@ -5,8 +5,7 @@ public class Cat {
 	public static void main(String[] args) {
 		System.out.println("eat");
 		System.out.println("walk");
-		System.out.println("this is viaj cay");
-		System.out.println("Addedd JDBC");
+		
 
 	}
 
